5bd12259fef68017357ff279bde80e5af7c6ef71
Sultan <rakhmetbekovsultan@gmail.com>
2026-08-19 00:25:38 +0500

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,38 +1,37 @@
 import allure
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from data import BASE_URL  # Импортируем URL из файла с данными
 
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.base_url = BASE_URL
+        self.base_url = "https://qa-scooter.praktikum-services.ru/"
 
-    @allure.step('Ожидание и поиск элемента')
+    @allure.step("Найти элемент с ожиданием")
     def find_element_with_wait(self, locator, timeout=10):
         return WebDriverWait(self.driver, timeout).until(
             EC.presence_of_element_located(locator)
         )
 
-    @allure.step('Клик по элементу')
+    @allure.step("Кликнуть по элементу")
     def click_element(self, locator, timeout=10):
         element = WebDriverWait(self.driver, timeout).until(
             EC.element_to_be_clickable(locator)
         )
         element.click()
 
-    @allure.step('Ввод текста в поле')
+    @allure.step("Ввести текст")
     def input_text(self, locator, text, timeout=10):
         element = self.find_element_with_wait(locator, timeout)
         element.send_keys(text)
 
-    @allure.step('Скролл до элемента')
+    @allure.step("Проскроллить до элемента")
     def scroll_to_element(self, locator):
         element = self.find_element_with_wait(locator)
         self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
 
-    @allure.step('Получение текста элемента')
+    @allure.step("Получить текст элемента")
     def get_text(self, locator, timeout=10):
         element = self.find_element_with_wait(locator, timeout)
         return element.text
